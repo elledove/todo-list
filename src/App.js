e8e360ed24d2ac49e94ddf8f9e3a70c671bd96ec
Lauren Dove <elledoveinc@gmail.com>
2020-11-11 12:06:11 -0500

    deleted extra files.turned App.js into a class component with constructor and super.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Halo Boss!</h1>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      newItem:"",
+      list:[]
+     }
+  }
+  render() { 
+    return ( <div className="App">
+
+           <div>
+            Add an Item Here:
+            <br></br>
+            <input  type="text" value={this.state.newItem} placeholder="Lets do it!"></input>
+
+          </div>
+    </div>  );
+  }
 }
-
+ 
 export default App;
